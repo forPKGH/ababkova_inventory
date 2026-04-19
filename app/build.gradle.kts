c7fa4f7092da9e193e84e5f36fb80017026ce239
kirk741 <kir.gac.3@gmail.com>
2026-04-19 17:52:26 +0300

    Создан Entity-класс Item

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,9 +77,9 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.5")
 
     //Room
-    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
-    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
-    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
     // Testing
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
